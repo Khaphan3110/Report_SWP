c655a3a8340e4f16c464d82892ebd994ee34d307
Norman <khanhuchiha2k4@gmail.com>
2024-05-26 03:17:40 +0700

    Created login function on fake database

---
+++ SourceCode/Code_BackEnd/SWPSolution/EShopServices/IUserServices.cs
@@ -0,0 +1,20 @@
+﻿using EShopBusinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopServices
+{
+    public interface IUserService
+    {
+        public User GetUser(int id);
+        public List<User> GetUsers();
+        public User FindUserid(int id);
+        public User AddUser(User user);
+        public User UpdateUser(int id, User user);
+        public void DeleteUser(int id);
+        public string GetLogin(string username, string password);
+    }
+}
---
+++ SourceCode/Code_BackEnd/SWPSolution/SWPSolution.ObjectRepositories/UserRepositories.cs
@@ -0,0 +1,55 @@
+﻿using EShopBusinessObject;
+using EShopDAOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EShopRepositories
+{
+    public class UserRepositories : IUserRepositories
+    {
+        private readonly UserDAO userDAO = null;
+        public UserRepositories()
+        {
+            if(userDAO == null)
+            {
+                userDAO = new UserDAO();
+            }
+        }
+        public User AddUser(User user)
+        {
+            return userDAO.AddUser(user);
+        }
+
+        public void DeleteUser(int id)
+        {
+            userDAO.DeleteUser(id);    
+        }
+
+        public User FindUserid(int id)
+        {
+            return userDAO.FindUserid(id);
+        }
+        public User GetUser(int id)
+        {
+            return userDAO.GetUser(id);
+        }
+
+        public List<User> GetUsers()
+        {
+            return userDAO.GetUsers();
+        }
+
+        public User UpdateUser(int id, User user)
+        {
+            return userDAO.UpdateUser(id, user);
+        }
+        public string GetLogin(string username, string password)
+        {
+            string response = userDAO.GetLogin(username, password);
+            return response;
+        }
+    }
+}
