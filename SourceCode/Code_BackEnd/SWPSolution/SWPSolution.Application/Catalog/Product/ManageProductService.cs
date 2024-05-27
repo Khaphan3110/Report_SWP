147fdb0806a06f0bf6710c2e0d026127d9477c72
Norman <khanhuchiha2k4@gmail.com>
2024-05-28 01:18:42 +0700

    started creating application layer functions

---
+++ SourceCode/Code_BackEnd/SWPSolution/SWPSolution.Application/Catalog/Product/ManageProductService.cs
@@ -0,0 +1,53 @@
+﻿using SWPSolution.Application.Catalog.Product.Dtos;
+using SWPSolution.Application.Dtos;
+using SWPSolution.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWPSolution.Application.Catalog.Product
+{
+    public class ManageProductService : IManageProductService
+    {
+        private readonly SWPSolutionDBContext _context;
+        public ManageProductService(SWPSolutionDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<int> Create(ProductCreateRequest request)
+        {
+            var product = new Data.Entities.Product
+            {
+                ProductName = request.ProductName,
+                Quantity = request.Quantity,
+                Price = request.Price,
+                Description = request.Description,
+
+            };
+            _context.Products.Add(product);
+            return await _context.SaveChangesAsync();
+        }
+
+        public async Task<int> Delete(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<PagedViewModel<ProductViewModel>> GetAllPagning(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<int> Update(UpdateProductEditRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
