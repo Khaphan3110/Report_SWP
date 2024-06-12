21debf5ac588f7f89959175a15b0b928a22ed2ef
KhaPhan31 <phanhoangduykha3@gmail.com>
2024-06-12 20:35:23 +0700

    hi

--- SourceCode/Code_FontEnd/code_fontend/src/App.js
+++ SourceCode/Code_FontEnd/code_fontend/src/App.js
@@ -1,36 +1,20 @@
 import { Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import Footer from "./Components/Footer/Footer";
-import ForgotPasswordForm from "./Components/ForgotPassword/ForgotPasswordForm";
-import SendEmailForm from "./Components/ForgotPassword/SendEmailForm";
-import HomePage from "./Components/HomePage/HomePage";
-import LoginForm from "./Components/Login/LoginForm";
-import RegisterForm from "./Components/Register/RegisterForm";
-import Header from "./Components/Header/Header";
-import AuthenEmail from "./Components/Authen/AuthenEmail";
-import Content from "./Components/Content/Content";
-import AccountPage from "./Components/AccountPage/AccountPage";
-import AddressPage from "./Components/AddressPage/AddressPage";
-import CartPage from "./Components/CartPage/CartPage";
-import ResetPasswordPage from "./Components/ForgotPassword/ResetPasswordPage";
-import CheckoutPage from "./Components/CheckoutPage/CheckoutPage";
-import PaymentPage from "./Components/PaymentPage/PaymentPage";
-=======
-import RouteAdminLayout from "./Layouts/AdminLayout/RouteAdminLayout";
-import RouteUserLayout from "./Layouts/UserLayouts/RouteUserLayout";
-import LoginForm from "./Components/User/Login/LoginForm";
-import Header from "./Components/User/Header/Header";
 import Footer from "./Components/User/Footer/Footer";
+import ForgotPasswordForm from "./Components/User/ForgotPassword/ForgotPasswordForm";
+import SendEmailForm from "./Components/User/ForgotPassword/SendEmailForm";
 import AccountPage from "./Components/User/AccountPage/AccountPage";
 import AddressPage from "./Components/User/AddressPage/AddressPage";
+import AuthenEmail from "./Components/User/Authen/AuthenEmail";
 import CartPage from "./Components/User/CartPage/CartPage";
 import Content from "./Components/User/Content/Content";
+import Header from "./Components/User/Header/Header";
+import LoginForm from "./Components/User/Login/LoginForm";
 import RegisterForm from "./Components/User/Register/RegisterForm";
-import AuthenEmail from "./Components/User/Authen/AuthenEmail";
-import SendEmailForm from "./Components/User/ForgotPassword/SendEmailForm";
-import ForgotPasswordForm from "./Components/User/ForgotPassword/ForgotPasswordForm";
+import RouteAdminLayout from "./Layouts/AdminLayout/RouteAdminLayout";
+import RouteUserLayout from "./Layouts/UserLayouts/RouteUserLayout";
+import CheckoutPage from "./Components/User/CheckoutPage/CheckoutPage";
+import PaymentPage from "./Components/User/PaymentPage/PaymentPage";
 
->>>>>>> 4fffd67ee0402449809fba2b15bd168b204aa625
 function App() {
   return (
     <>
@@ -50,7 +34,6 @@ function App() {
         <Route path="/authenOTP" element={<AuthenEmail />} />
         <Route path="/sendEmailForgot" element={<SendEmailForm />} />
         <Route path="/ForgotPassword" element={<ForgotPasswordForm />} />
-<<<<<<< HEAD
         <Route path="/reset-password/:token/:email" element={<ResetPasswordPage />} />
 =======
         <Route path="/CategoriesType" element={<ImportCategories/>} />
@@ -66,6 +49,8 @@ function App() {
           <Route path="/account" element={<AccountPage />} />
           <Route path="/addresses" element={<AddressPage />} />
           <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+          <Route path="/payment" element={<PaymentPage />} />
           <Route path="/Content" element={<Content />} />
           <Route path="/register" element={<RegisterForm />} />
           <Route path="/authenOTP" element={<AuthenEmail />} />
@@ -77,7 +62,6 @@ function App() {
         <Route path="/admin" element={<RouteAdminLayout />}>
           <Route path="register" element={<RegisterForm />} />
         </Route>
->>>>>>> 4fffd67ee0402449809fba2b15bd168b204aa625
       </Routes>
     </>
   );
