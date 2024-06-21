4ee8e53d013fa76edf12a3eb82132e25235c6537
Norman <87079855+normaanf@users.noreply.github.com>
2024-06-21 22:27:24 +0700

    Add .gitignore and update other files

--- SourceCode/Code_BackEnd/SWPSolution/SWPSolution.Data/obj/Debug/net8.0/SWPSolution.Data.AssemblyInfo.cs
+++ SourceCode/Code_BackEnd/SWPSolution/SWPSolution.Data/obj/Debug/net8.0/SWPSolution.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SWPSolution.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d2f856562a7ffb34da8252b342e455711d6416")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33911e1a32edb3197b301100afc05d95ee32044a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SWPSolution.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SWPSolution.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
