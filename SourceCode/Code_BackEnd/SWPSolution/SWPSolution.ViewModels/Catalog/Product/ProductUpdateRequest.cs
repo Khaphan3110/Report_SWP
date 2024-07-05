57a7eab6dc71b5762b5271a6607979cb03695163
Norman <khanhuchiha2k4@gmail.com>
2024-07-05 16:07:17 +0700

    fix product update

--- SourceCode/Code_BackEnd/SWPSolution/SWPSolution.ViewModels/Catalog/Product/ProductUpdateRequest.cs
+++ SourceCode/Code_BackEnd/SWPSolution/SWPSolution.ViewModels/Catalog/Product/ProductUpdateRequest.cs
@@ -3,16 +3,14 @@
 namespace SWPSolution.ViewModels.Catalog.Product
 {
     public class ProductUpdateRequest
-    {
-        public string ProductId { get; set; }
-
-        public string CategoriesId { get; set; }
-
+    { 
         public string ProductName { get; set; }
 
         public int? Quantity { get; set; }
 
         public string Description { get; set; }
+
+        public string StatusDescription { get; set; }
 
         public IFormFile ThumbnailImage { get; set; }
     }
